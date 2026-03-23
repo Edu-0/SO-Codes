4c86eb667c1388569db574c06a9e37d3330f590c
Edu-0 <eduardo.8288917@edu.univali.br>
2026-03-22 21:35:55 -0300

    docs: added a bunch of comments explaining

--- IPC/shm_linux_consumer.c
+++ IPC/shm_linux_consumer.c
@@ -21,27 +21,36 @@ int main(){
 	int i;
 
 	/* open the shared memory segment */
-	shm_fd = shm_open(name, O_RDONLY, 0666);
+	shm_fd = shm_open(name, O_RDONLY, 0666); // O_RDONLY: Open the shared memory object for reading only. This means that the consumer process will only be able to read data from the shared memory, and it will not have permission to write to it. The producer process, on the other hand, can open the shared memory object with O_RDWR to allow both reading and writing.
 	if (shm_fd == -1) {
 		printf("shared memory failed\n");
 		exit(-1);
 	}
 
 	/* now map the shared memory segment in the address space of the process */
-	ptr = mmap(0,SIZE, PROT_READ, MAP_SHARED, shm_fd, 0);
+	ptr = mmap(0,SIZE, PROT_READ, MAP_SHARED, shm_fd, 0); // PROT_READ: This flag indicates that the memory region should be mapped with read-only permissions. This means that the consumer process will only be able to read data from the shared memory, and it will not have permission to write to it. The producer process can map the shared memory region with PROT_READ | PROT_WRITE to allow both reading and writing.
 	if (ptr == MAP_FAILED) {
 		printf("Map failed\n");
 		exit(-1);
 	}
 
 	/* now read from the shared memory region */
-	printf("%s",(char *)ptr);
+	printf("%s",(char *)ptr); // The consumer process reads the data from the shared memory region by dereferencing the pointer ptr. Since ptr is a void pointer, we need to cast it to a char pointer (char *) to read it as a string. The printf function then prints the contents of the shared memory, which should be the messages written by the producer process.
 
 	/* remove the shared memory segment */
-	if (shm_unlink(name) == -1) {
+	if (shm_unlink(name) == -1) { // shm_unlink() is used to remove the shared memory object from the system. It takes the name of the shared memory object as an argument. If the function returns -1, it indicates that there was an error in removing the shared memory object, and we print an error message and exit with a non-zero status.
 		printf("Error removing %s\n",name);
 		exit(-1);
 	}
 
+	/*
+		shm_unlink irá deletar todo aquele espaço de memória compartilhado que foi criado quando mais nenhum processo estiver usando ele. Se um processo ainda estiver usando o shared memory, ele ainda poderá acessar os dados.
+		Quando todos os processos fecham e shm_unlink é chamado, tudo é destruído.
+	*/
+
 	return 0;
 }
+
+/*
+	Primeiramente eu rodo o producer, cria o seguimento de memória compartilhado, escreve as mensagens e depois rodo o consumer, que lê as mensagens do segmento de memória compartilhado e depois de ler, o consumer chama shm_unlink() para remover o segmento de memória.
+*/
--- IPC/shm_linux_producer.c
+++ IPC/shm_linux_producer.c
@@ -26,13 +26,41 @@ int main(){
 
 	/* create the shared memory segment */
 	shm_fd = shm_open(name, O_CREAT | O_RDWR, 0666);
+	/*
+		shm_open() creates a new shared memory object or opens an existing shared memory object.
+		The name argument specifies the name of the shared memory object. It must begin with a slash
+		O_CREAT: If the shared memory object does not exist, it will be created. If it already exists, this flag has no effect.
+		O_RDWR: Open the shared memory object for reading and writing.
+		0666: This sets the permissions for the shared memory object, allowing read and write
+	*/
 
 	/* configure the size of the shared memory segment */
-	ftruncate(shm_fd,SIZE);
+	ftruncate(shm_fd,SIZE); // ftruncate() is used to set the size of the shared memory object. It takes two arguments: the file descriptor of the shared memory object and the desired size in bytes. In this case, we set the size to 4096 bytes (4 KB).
+
+
+	/*
+		Um cria/abre o objeto de memória compartilhada usando shm_open()
+		Configura após o seu tamanho com ftruncate()
+		Depois mapeia o segmento de memória compartilhada no espaço de endereçamento do processo usando mmap(). 
+		O produtor então escreve mensagens na região de memória compartilhada, incrementando o ponteiro após cada escrita para garantir que as mensagens sejam armazenadas corretamente.
+	*/
+
 
 	/* now map the shared memory segment in the address space of the process */
-	ptr = mmap(0,SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, shm_fd, 0);
-	if (ptr == MAP_FAILED) {
+	ptr = mmap(0,SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, shm_fd, 0); // ptr vai ser a referência para o início da região de memória compartilhada mapeada no espaço de endereçamento do processo.
+
+	/*
+		0 - The starting address for the mapping.
+		SIZE - The max size of the mapping (4096 bytes).
+		PROT_READ | PROT_WRITE - The desired memory protection of the mapping, which allows both reading and writing to the shared memory.
+		MAP_SHARED - This flag indicates that updates to the mapping are visible to other processes that map the same shared memory object, and that changes are shared between processes.
+		shm_fd - The file descriptor of the shared memory object obtained from shm_open().
+		0 - The offset in the shared memory object where the mapping starts. In this case, it is set to 0, meaning the mapping starts at the beginning of the shared memory object.
+
+		File Descriptor = Isso aqui vai ser uma referência no qual o SO vai usar para trabalhar com meus pedidos de separar um espaço para shared memory
+	*/
+
+	if (ptr == MAP_FAILED) { // MAP_FAILED é uma constante que indica que a função mmap() falhou ao mapear a memória compartilhada. Se ptr for igual a MAP_FAILED, significa que ocorreu um erro durante o mapeamento.
 		printf("Map failed\n");
 		return -1;
 	}
@@ -42,8 +70,11 @@ int main(){
  	 *
 	 * Note we must increment the value of ptr after each write.
 	 */
-	sprintf(ptr,"%s",message0);
-	ptr += strlen(message0);
+	sprintf(ptr,"%s",message0); // sprintf() is a function that formats and stores a series of characters and values in the buffer pointed to by ptr. In this case, it writes the string message0 ("Studying ") into the shared memory region pointed to by ptr.
+	ptr += strlen(message0); // After writing message0, we increment the pointer ptr by the length of message0 using strlen(message0). This moves the pointer to the position immediately after the end of the written message, allowing us to write the next message without overwriting the previous one.
+	// Each char is 1 byte, so in the end with message0 we will have 10 bytes used in the shared memory, and ptr will point to the 11th byte, which is where we will write message1.
+	// \0 is the null terminator that marks the end of a string in C. When we write message0 to the shared memory, it includes the null terminator at the end of the string. So, after writing message0, we have 10 bytes used (9 characters + 1 null terminator), and ptr will point to the 11th byte, which is where we will write message1.
+	// It is done so that when the consumer process reads the shared memory, it can correctly identify the end of each message using the null terminator.
 	sprintf(ptr,"%s",message1);
 	ptr += strlen(message1);
 	sprintf(ptr,"%s",message2);
